ee39fafec4640d76b69464b39d98cc1047c3f6ac
Akshay4504 <akshayanand4504@gmail.com>
2026-06-19 12:31:36 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.java
@@ -0,0 +1,27 @@
+//Brute-Force
+// public class Solution {
+//     public boolean hasDuplicate(int[] nums) {
+//         for (int i = 0; i < nums.length; i++) {
+//             for (int j = i + 1; j < nums.length; j++) {
+//                 if (nums[i] == nums[j]) {
+//                     return true;
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+// }
+
+//Optimized
+public class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                return true;
+            }
+            seen.add(num);
+        }
+        return false;
+    }
+}
